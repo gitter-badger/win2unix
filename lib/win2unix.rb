955f7f9dbefc1e4ec20ba11173c4d7ee43f8d9f4
liuxd <liuxidong1984@gmail.com>
2015-05-27 21:18:37 +0800

    travel all files

--- lib/win2unix.rb
+++ lib/win2unix.rb
@@ -1,5 +1,30 @@
 require "win2unix/version"
 
 module Win2unix
-  # Your code goes here...
+  @@file_list = []
+
+  def self.tidy path, ext = 'txt'
+  	getAllFiles path, ext
+    @@file_list.each do |file|
+      handle file
+    end
+
+    @@file_list.size
+  end
+
+  def self.getAllFiles path, ext = 'txt'
+  	Dir.glob("#{path}/*") do |file|
+  	  if File.directory? file
+  		  getAllFiles file
+  	  else
+  		  if File.extname(file) == '.' + ext
+          @@file_list.push file
+        end
+  	  end
+  	end
+  end
+
+  def self.handle file
+    p file
+  end
 end
